b33aaada180083657df2dbd6d2215091cd23e07b
Soe Htet Linn <mgsoehtetlinn@gmail.com>
2025-01-09 12:03:19 +0700

    remove validation

--- cd_account_management/models/account_move.py
+++ cd_account_management/models/account_move.py
@@ -216,6 +216,18 @@ class AccountMoveLine(models.Model):
     entity_num = fields.Integer('Entity Number')
     working_date = fields.Date('Working Date')
     court_date = fields.Date('Court Date')
+    
+    @api.constrains('account_id', 'display_type')
+    def _check_payable_receivable(self):
+        # for line in self:
+        #     account_type = line.account_id.account_type
+        #     if line.move_id.is_sale_document(include_receipts=True):
+        #         if (line.display_type == 'payment_term') ^ (account_type == 'asset_receivable'):
+        #             raise UserError(_("Any journal item on a receivable account must have a due date and vice versa."))
+        #     if line.move_id.is_purchase_document(include_receipts=True):
+        #         if (line.display_type == 'payment_term') ^ (account_type == 'liability_payable'):
+        #             raise UserError(_("Any journal item on a payable account must have a due date and vice versa."))
+        return True
 
 class AccountAccount(models.Model):
     _inherit = 'account.account'
